baca10e2026a5392d73d96b53a73bd9edbe97297
xdissent <xdissent@gmail.com>
2009-07-07 17:45:41 -0500

    Removed dictionary access for request. Fixed undeclared variable issue in profile panel. Added profiling link.

--- debug_toolbar/panels/profile.py
+++ debug_toolbar/panels/profile.py
@@ -11,32 +11,38 @@ except:
 class ProfileDebugPanel(DebugPanel):
     has_content = True
     name = 'Profile'
+    
+    def __init__(self):
+        self.profiler = None
+        
     def title(self):
         return 'Profile'
 
     def url(self):
         return ''
+        
     def process_view(self, request, view_func, view_args, view_kwargs):
-        logging.debug("processing view")
-        if request.has_key('prof'):
-            logging.debug("have key")
+        logging.debug("Processing view")
+        if request.REQUEST.has_key('prof'):
+            logging.debug("Profiler key found in request")
             self.profiler = profile.Profile()
-            #args = (request,) + view_args
             return self.profiler.runcall(view_func, request, *view_args, **view_kwargs)
+            
     def content(self):
-        #self.profiler.create_stats()
-        out = StringIO()
-        old_stdout, sys.stdout = sys.stdout, out
+        content = None
+        if self.profiler is not None:
+            out = StringIO()
+            old_stdout, sys.stdout = sys.stdout, out
         
-        self.tmpfile = tempfile.mktemp()
-        self.profiler.dump_stats(self.tmpfile)
-        stats = pstats.Stats(self.tmpfile)
+            self.tmpfile = tempfile.mktemp()
+            self.profiler.dump_stats(self.tmpfile)
+            stats = pstats.Stats(self.tmpfile)
         
-        stats.sort_stats('time')
-        stats.print_stats(.1)
+            stats.sort_stats('time')
+            stats.print_stats(.1)
         
-        sys.stdout = old_stdout
-        content = '<pre>%s</pre>' % out.getvalue()
+            sys.stdout = old_stdout
+            content = '<pre>%s</pre>' % out.getvalue()
         return render_to_string('debug_toolbar/panels/profile.html', {'content':content})
         
         
